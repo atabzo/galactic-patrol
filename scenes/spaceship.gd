39129481b27d7765c7da8ee399c44f12fd560ae4
atabzo <myemail.bhanu@gmail.com>
2026-01-23 03:49:01 +0530

    added a custom font

--- scenes/spaceship.gd
+++ scenes/spaceship.gd
@@ -8,5 +8,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position += Vector2(0,-1) * 10
+	position += Vector2(0,-1) * 1
 	pass
+	
+	
