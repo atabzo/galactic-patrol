510cd7953c6dd762e9fb37d971a001cb6c8bffc2
atabzo <myemail.bhanu@gmail.com>
2026-01-23 09:50:53 +0530

    added an ending + wip scene!

---
+++ reactive-stars.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+var sprite: Sprite2D
+var original_scale: Vector2
+var hover_scale: Vector2
+
+func _ready():
+	sprite = $DecorativeStars
+	original_scale = sprite.scale
+	hover_scale = sprite.scale * 1.1
+	
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+
+func _on_mouse_entered():
+	var tween = create_tween()
+	tween.tween_property(sprite, "scale", hover_scale, 0.2).set_trans(Tween.TRANS_SINE)
+
+func _on_mouse_exited():
+	var tween = create_tween()
+	tween.tween_property(sprite, "scale", original_scale, 0.2).set_trans(Tween.TRANS_SINE)
---
+++ scenes/ending-scene-button.gd
@@ -0,0 +1,10 @@
+extends Button
+
+# Path to the scene you want to load
+@export var next_scene: String = "res://scenes/page2.tscn"
+
+func _ready():
+	pressed.connect(_on_button_pressed)
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file(next_scene)
