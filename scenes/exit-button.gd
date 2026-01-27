ea4011e06078b3f1e64231187baabfc0dddca3eb
atabzo <myemail.bhanu@gmail.com>
2026-01-27 16:54:14 +0530

    cleaned up some unnecessary scripts

--- scenes/exit-button.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-@export var next_scene: String = "res://scenes/ending_scene.tscn"
-
-func _ready():
-	pressed.connect(_on_button_pressed)
-	
-func _on_button_pressed():
-	get_tree().change_scene_to_file(next_scene)
